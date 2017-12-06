b33f9ab8489569d8e24ba0abcf27d87ed5734f1c
Glen Michael Austin Eder <geder@purdue.edu>
2017-12-06 13:01:25 -0500

    Adds partA of day 4

---
+++ Day4/partA.py
@@ -0,0 +1,22 @@
+with open("input.txt") as f:
+	data = f.read()
+
+isVaild = True
+totalValid = 0
+lines = data.split('\n')
+linesLength = len(lines)
+
+for l in range(linesLength):
+	isVaild = True
+	words = lines[l].split(" ")
+	for i in range(len(words)):
+		for k in range(len(words)):
+			if i != k and words[i] == words[k]:
+				isVaild = False
+
+	if isVaild:
+		totalValid = totalValid + 1
+
+
+
+print(totalValid)
